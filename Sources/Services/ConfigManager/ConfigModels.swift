59ad48add7ad6bc41d8d9c0b1f7def2be87d800d
Aromatic <symwww@outlook.com>
2025-07-24 22:07:32 +0800

    refactor: reorganize AppConfig and SearchDirectory structures for improved clarity and maintainability

--- Sources/Services/ConfigManager/ConfigModels.swift
+++ Sources/Services/ConfigManager/ConfigModels.swift
@@ -1,3 +1,6 @@
+import Foundation
+import Carbon
+
 // 默认配置常量
 enum AppConfigDefaults {
     static let searchDirectories: [SearchDirectory] = [
@@ -34,33 +37,36 @@ enum AppConfigDefaults {
         "git": ["github desktop", "sourcetree"],
         "vm": ["vmware", "parallels"],
     ]
-}
-import Foundation
-import Carbon
 
-// 搜索目录数据结构
-struct SearchDirectory: Identifiable, Codable, Hashable {
-    var id: String { path } // 使用路径作为 ID，确保稳定性
-    let path: String
-    
-    init(path: String) {
-        self.path = path
-    }
-    
-    // 自定义编码，只保存路径
-    private enum CodingKeys: String, CodingKey {
-        case path
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.path = try container.decode(String.self, forKey: .path)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(path, forKey: .path)
-    }
+    static let modeEnabled: [String: Bool] = Dictionary(uniqueKeysWithValues: AppConfig.ModesConfig.allModes.map { ($0, true) })
+    static let showCommandSuggestions: Bool = true
+    static let defaultSearchEngine: String = "google"
+    static let preferredTerminal: String = "auto"
+    static let enabledBrowsers: [String] = ["safari"]
+    static let fileBrowserStartPaths: [String] = [
+        NSHomeDirectory(),
+        NSHomeDirectory() + "/Desktop",
+        NSHomeDirectory() + "/Downloads",
+        NSHomeDirectory() + "/Documents"
+    ]
+
+    static let modes: AppConfig.ModesConfig = AppConfig.ModesConfig(
+        enabled: modeEnabled,
+        showCommandSuggestions: showCommandSuggestions,
+        defaultSearchEngine: defaultSearchEngine,
+        preferredTerminal: preferredTerminal,
+        enabledBrowsers: enabledBrowsers,
+        fileBrowserStartPaths: fileBrowserStartPaths
+    )
+
+    static let hotKey: AppConfig.HotKeyConfig = AppConfig.HotKeyConfig()
+
+    static let defaultConfig: AppConfig = AppConfig(
+        hotKey: hotKey,
+        searchDirectories: searchDirectories,
+        commonAbbreviations: commonAbbreviations,
+        modes: modes
+    )
 }
 
 // 配置数据结构
@@ -87,23 +93,25 @@ struct AppConfig: Codable {
         var preferredTerminal: String
         var enabledBrowsers: [String]
         var fileBrowserStartPaths: [String]
-        
+
         static let allModes: [String] = [
             "kill", "search", "web", "terminal", "file", "clip", "plugin", "launch"
         ]
-        
-        init() {
-            self.enabled = Dictionary(uniqueKeysWithValues: Self.allModes.map { ($0, true) })
-            self.showCommandSuggestions = true
-            self.defaultSearchEngine = "google"
-            self.preferredTerminal = "auto"
-            self.enabledBrowsers = ["safari"]
-            self.fileBrowserStartPaths = [
-                NSHomeDirectory(),
-                NSHomeDirectory() + "/Desktop",
-                NSHomeDirectory() + "/Downloads",
-                NSHomeDirectory() + "/Documents"
-            ]
+
+        init(
+            enabled: [String: Bool] = AppConfigDefaults.modeEnabled,
+            showCommandSuggestions: Bool = AppConfigDefaults.showCommandSuggestions,
+            defaultSearchEngine: String = AppConfigDefaults.defaultSearchEngine,
+            preferredTerminal: String = AppConfigDefaults.preferredTerminal,
+            enabledBrowsers: [String] = AppConfigDefaults.enabledBrowsers,
+            fileBrowserStartPaths: [String] = AppConfigDefaults.fileBrowserStartPaths
+        ) {
+            self.enabled = enabled
+            self.showCommandSuggestions = showCommandSuggestions
+            self.defaultSearchEngine = defaultSearchEngine
+            self.preferredTerminal = preferredTerminal
+            self.enabledBrowsers = enabledBrowsers
+            self.fileBrowserStartPaths = fileBrowserStartPaths
         }
         
         init(from decoder: Decoder) throws {
@@ -111,28 +119,13 @@ struct AppConfig: Codable {
             if let enabled = try? container.decode([String: Bool].self, forKey: .enabled) {
                 self.enabled = enabled
             } else {
-                var dict: [String: Bool] = [:]
-                dict["kill"] = (try? container.decode(Bool.self, forKey: .killModeEnabled)) ?? true
-                dict["search"] = (try? container.decode(Bool.self, forKey: .searchModeEnabled)) ?? true
-                dict["web"] = (try? container.decode(Bool.self, forKey: .webModeEnabled)) ?? true
-                dict["terminal"] = (try? container.decode(Bool.self, forKey: .terminalModeEnabled)) ?? true
-                dict["file"] = (try? container.decode(Bool.self, forKey: .fileModeEnabled)) ?? true
-                dict["clip"] = true
-                dict["plugin"] = true
-                dict["launch"] = true
-                self.enabled = dict
+                self.enabled = AppConfigDefaults.modeEnabled
             }
-            self.showCommandSuggestions = (try? container.decode(Bool.self, forKey: .showCommandSuggestions)) ?? true
-            self.defaultSearchEngine = (try? container.decode(String.self, forKey: .defaultSearchEngine)) ?? "google"
-            self.preferredTerminal = (try? container.decode(String.self, forKey: .preferredTerminal)) ?? "auto"
-            self.enabledBrowsers = (try? container.decode([String].self, forKey: .enabledBrowsers)) ?? ["safari"]
-            self.fileBrowserStartPaths = (try? container.decode([String].self, forKey: .fileBrowserStartPaths)) ?? [
-                NSHomeDirectory(),
-                NSHomeDirectory() + "/Desktop",
-                NSHomeDirectory() + "/Downloads",
-                NSHomeDirectory() + "/Documents",
-                "/"
-            ]
+            self.showCommandSuggestions = (try? container.decode(Bool.self, forKey: .showCommandSuggestions)) ?? AppConfigDefaults.showCommandSuggestions
+            self.defaultSearchEngine = (try? container.decode(String.self, forKey: .defaultSearchEngine)) ?? AppConfigDefaults.defaultSearchEngine
+            self.preferredTerminal = (try? container.decode(String.self, forKey: .preferredTerminal)) ?? AppConfigDefaults.preferredTerminal
+            self.enabledBrowsers = (try? container.decode([String].self, forKey: .enabledBrowsers)) ?? AppConfigDefaults.enabledBrowsers
+            self.fileBrowserStartPaths = (try? container.decode([String].self, forKey: .fileBrowserStartPaths)) ?? AppConfigDefaults.fileBrowserStartPaths
         }
         
         func encode(to encoder: Encoder) throws {
@@ -150,6 +143,30 @@ struct AppConfig: Codable {
             case killModeEnabled, searchModeEnabled, webModeEnabled, terminalModeEnabled, fileModeEnabled
             case showCommandSuggestions, defaultSearchEngine, preferredTerminal, enabledBrowsers, fileBrowserStartPaths
         }
+    }
+}
+
+// 搜索目录数据结构
+struct SearchDirectory: Identifiable, Codable, Hashable {
+    var id: String { path } // 使用路径作为 ID，确保稳定性
+    let path: String
+    
+    init(path: String) {
+        self.path = path
+    }
+    // 自定义编码，只保存路径
+    private enum CodingKeys: String, CodingKey {
+        case path
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.path = try container.decode(String.self, forKey: .path)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(path, forKey: .path)
     }
 }
 
